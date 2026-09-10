d27fc04a99379a5dafcaf2008246447bf5969290
Williams Adele <williams.adele.dev@gmail.com>
2026-09-10 13:24:06 +0100

    Practicing th higher order functions and arrays

--- country-list.js
+++ country-list.js
@@ -1,3 +1,24 @@
-let country_list = {
-    
-}
+// const canDrink = ages.filter((age) => {
+//     if(age >= 21){
+//         return true
+//     }
+//     else {return false
+
+//     }
+// })
+
+const companies = [
+    {name: "January", category: "Retatil"},
+    {name: "Febuary", category: "Distributor"},
+    {name: "March", category: "Retatil"},
+    {name: "April", category: "Wholesale"},
+]
+const getRetails = companies.filter(company =>{ 
+    if(company.category === "Retail"){
+        return true
+    }
+});
+console.log(getRetails)
+// const getCategory = companies.filter((company) => company.category === "Retail")
+
+const eightiesCompanies = companies.filter(company => (company.start >= 1980 && company.start < 1990))
