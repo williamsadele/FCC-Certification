0fc3d208531542a272c71911c14058846bd43873
Williams Adele <williams.adele.dev@gmail.com>
2026-09-10 15:41:26 +0100

    finished finance tracker app, used higher order array methods(.filter, .reduce) to calculate income, expenses and total

--- FT.js
+++ FT.js
@@ -20,23 +20,39 @@ const addTransaction = (event) => {
         amount: Number(amountInput.value.trim())
     };
     transactions.push(transaction)
-    renderlist()
+    render(transactions, list)
+    updateTotals()
     textInput.value = "";
     amountInput.value = "";
 }
 form.addEventListener("submit", addTransaction)
 
-const renderlist = () => {
+const render = (array, hist) => {
     let history = ""
-    for(let i=0; i<transactions.length; i++) {
-        const sign = transactions[i].amount < 0 ? "-" : "+";
-        const itemClass = transactions[i].amount < 0 ? "minus" : "plus"
+    for(let i=0; i<array.length; i++) {
+        const sign = array[i].amount < 0 ? "-" : "+";
+        const itemClass = array[i].amount < 0 ? "minus" : "plus"
         history += `
         <li class="${itemClass}">
-        ${transactions[i].text}
-        <span>${sign}$${Math.abs(transactions[i].amount)}</span>
+        ${array[i].text}
+        <span>${sign}$${Math.abs(array[i].amount)}</span>
         </li>
         `
     }
-    list.innerHTML = history
+    hist.innerHTML = history
+}
+const updateTotals = () => {
+    const income = transactions
+        .filter(item => item.amount > 0)
+        .reduce((acc, item) => acc + item.amount, 0)
+
+    const expense = transactions
+        .filter(item => item.amount < 0)
+        .reduce((acc, item) => acc + item.amount, 0)
+
+    const total = transactions.reduce((acc, item) => acc + item.amount, 0)
+
+    budgetBalance.textContent = `$${total.toFixed(2)}`
+    moneyPlus.textContent = `+$${income.toFixed(2)}`
+    moneyMinus.textContent = `-$${Math.abs(expense).toFixed(2)}`
 }
