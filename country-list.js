368f7bb33c727e8553bee349f4af62e6fe8ecbb1
Williams Adele <williams.adele.dev@gmail.com>
2026-09-10 13:48:54 +0100

    .reduce still confusing me

--- country-list.js
+++ country-list.js
@@ -13,12 +13,26 @@ const companies = [
     {name: "March", category: "Retatil"},
     {name: "April", category: "Wholesale"},
 ]
-const getRetails = companies.filter(company =>{ 
-    if(company.category === "Retail"){
-        return true
-    }
-});
-console.log(getRetails)
-// const getCategory = companies.filter((company) => company.category === "Retail")
+// const getRetails = companies.filter(company =>{ 
+//     if(company.category === "Retail"){
+//         return true
+//     }
+// });
+// console.log(getRetails)
+// // const getCategory = companies.filter((company) => company.category === "Retail")
 
-const eightiesCompanies = companies.filter(company => (company.start >= 1980 && company.start < 1990))
+// const eightiesCompanies = companies.filter(company => (company.start >= 1980 && company.start < 1990))
+// //Get companies that lasted 10 years or more
+// const tenYears = companies.filter(company => {
+//     if(company.end - company.start >= 10){
+//         return true
+//     }
+// })
+// const tenYears = companies.filter(company => (company.end - company.start >= 10))
+const companyNames = companies.map((company) => {
+    return company.name
+})
+console.log(companyNames)
+const ageSum = ages.reduce((total, age) => total + age, 0)
+
+const totalYears = companies.reduce((total, company) => total + (company.end - company.start) ,0)
